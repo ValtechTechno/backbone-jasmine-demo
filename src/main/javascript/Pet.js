9daa3e8353ab3b47d767ae74a9ea8ba7309bc599
Grégory PAUL <gregory.paul@valtech.fr>
2012-08-02 16:10:39 +0200

    Implementing a basic view to make test pass - GREEN

--- src/main/javascript/Pet.js
+++ src/main/javascript/Pet.js
@@ -3,3 +3,10 @@ var Pet = Backbone.Model.extend({});
 var Pets = Backbone.Collection.extend({
 	model: Pet
 });
+
+var PetsView = Backbone.View.extend({
+	el:'#pets',
+	initialize: function(collection) {
+		this.col = collection;
+	}
+});
